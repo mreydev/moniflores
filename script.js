8d4f6ee3bf2f93e3ae2e30d35d8e56cde20ae351
Marco Reyes <114314498+mreydev@users.noreply.github.com>
2024-03-22 02:35:49 -0600

    Actualizar script.js
    ejemplo

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 
 
+const playSound = function() {
+    let element = document.createElement('div');
+    element.setAttribute('style', 'display: none');
+    element.innerHTML = `
+      <audio autoplay loop>
+        <source src="archivo-audio.ogg" type="audio/ogg">
+        <source src="archivo-audio.mp3" type="audio/mpeg">
+      </audio>
+    `;
+    document.body.appendChild(element);
+    document.removeEventListener('click', playSound);
+}
 
+document.addEventListener('click', playSound
 
 onload = () => {
 
